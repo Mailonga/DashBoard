bbd91838fe38dc900ec2b791214f80c6503b69cc
Mailonga Albuquerque <81332475+Mailonga@users.noreply.github.com>
2022-09-20 15:13:47 -0300

    Atualizações Finais!

---
+++ Order.js
@@ -0,0 +1,45 @@
+const Orders = [
+    {
+    productName: 'Fordable Mini Drone',
+    productNumber: '47521',
+    paymentStatus: 'EM Espera',
+    shipping: 'Pendente',
+    },
+    {
+    productName: 'Lavander KF102 Drone',
+    productNumber: '35214',
+    paymentStatus: 'Retornado',
+    shipping: 'Cancelado',
+    },
+    {
+    productName: 'Ruko F11 Pro Drone',
+    productNumber: '48654',
+    paymentStatus: 'EM Espera',
+    shipping: 'Pendente',
+    },
+    {
+    productName: 'Drone With Camera Drone',
+    productNumber: '95633',
+    paymentStatus: 'Pago',
+    shipping: 'Entregue',
+    },
+    {
+    productName: 'GPS 4k Drone',
+    productNumber: '24531',
+    paymentStatus: 'Pago',
+    shipping: 'Entregue',
+    },
+    {
+    productName: 'DJ Air 2S',
+    productNumber: '84572',
+    paymentStatus: 'Em Espera',
+    shipping: 'Pendente',
+    },
+    {
+    productName: 'Lozenge Drone',
+    productNumber: '00421',
+    paymentStatus: 'Pago',
+    shipping: 'Entregue',
+    }
+
+]
